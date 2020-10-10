fe533968f11d8778284855f1ba3e7dccf56895b3
RZ <52314673@qq.com>
2020-10-10 20:38:15 +0800

    我的java

---
+++ src/main/java/hello.java
@@ -0,0 +1,14 @@
+import javax.activation.MailcapCommandMap;
+
+/**
+ * Topic:
+ *
+ * @author RZ
+ * @date 2020/10/10 20:37
+ * Description:
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("java");
+    }
+}
