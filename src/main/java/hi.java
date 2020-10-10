3b706353008311ab138aebe87bc49b8741618f8b
RZ <52314673@qq.com>
2020-10-10 12:12:00 +0800

    Initial commit

---
+++ src/main/java/hi.java
@@ -0,0 +1,12 @@
+/**
+ * Topic:
+ *
+ * @author RZ
+ * @date 2020/10/10 12:01
+ * Description:
+ */
+public class hi {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
